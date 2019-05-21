fd09017a3c972d2f306c4ec96b864553d8459ef2
wongoo <gelnyang@163.com>
2019-05-21 11:26:48 +0800

    add license header

--- request_test.go
+++ request_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 ~ 2019, dubbogo.
+// Copyright 2016-2019 Alex Stocks, Yincheng Fang
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
