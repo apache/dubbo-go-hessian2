f3aa5467cf44c302f6330886b4a877971f0d2f54
aliiohs <rzy1107@163.com>
2019-07-06 23:22:52 +0800

    fix

--- test_hessian/src/main/java/test/TestCustomDecode.java
+++ test_hessian/src/main/java/test/TestCustomDecode.java
@@ -25,8 +25,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.math.BigDecimal;
-
 
 public class TestCustomDecode {
 
--- test_hessian/src/main/java/test/TestCustomReply.java
+++ test_hessian/src/main/java/test/TestCustomReply.java
@@ -22,7 +22,7 @@ import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
-
+import java.math.BigDecimal;
 
 public class TestCustomReply {
 
