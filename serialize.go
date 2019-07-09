7b15c9ad47982a3dab4b4680eae604cce85ccc81
aliiohs <rzy1107@163.com>
2019-07-09 10:01:53 +0800

    change license header author

--- serialize.go
+++ serialize.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019 Alex Stocks, Xinge Gao
+// Copyright 2016-2019 aliiohs
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
