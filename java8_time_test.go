97e42f605bd968b189a23923f24251414aea8730
chenyuebo <chenyuebo@sdiread.com>
2020-08-30 20:27:35 +0800

    add localdateatime add second, nano

--- java8_time_test.go
+++ java8_time_test.go
@@ -26,7 +26,7 @@ func TestJava8Time(t *testing.T) {
 	doTestTime(t, "java8_Year", &java8_time.Year{Year: 2020})
 	doTestTime(t, "java8_LocalDate", &java8_time.LocalDate{Year: 2020, Month: 6, Day: 6})
 	doTestTime(t, "java8_LocalTime", &java8_time.LocalTime{Hour: 6, Minute: 6})
-	doTestTime(t, "java8_LocalDateTime", &java8_time.LocalDateTime{Date: java8_time.LocalDate{Year: 2020, Month: 6, Day: 6}, Time: java8_time.LocalTime{Hour: 6, Minute: 6}})
+	doTestTime(t, "java8_LocalDateTime", &java8_time.LocalDateTime{Date: java8_time.LocalDate{Year: 2020, Month: 6, Day: 6}, Time: java8_time.LocalTime{Hour: 6, Minute: 6, Second: 6, Nano: 6}})
 	doTestTime(t, "java8_MonthDay", &java8_time.MonthDay{Month: 6, Day: 6})
 	doTestTime(t, "java8_Duration", &java8_time.Duration{Second: 0, Nano: 0})
 	doTestTime(t, "java8_Instant", &java8_time.Instant{Seconds: 100, Nanos: 0})
