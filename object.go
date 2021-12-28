9079120320bfb624d8d625565be64e016fd38717
望哥 <gelnyang@163.com>
2021-12-28 11:31:15 +0800

    format code

--- object.go
+++ object.go
@@ -473,7 +473,7 @@ func (d *Decoder) decInstance(typ reflect.Type, cls *classInfo) (interface{}, er
 			if !ok {
 				return nil, perrors.Errorf("value convert to bool failed, field name:%s", fieldName)
 			}
-			
+
 			if fldRawValue.Kind() == reflect.Ptr && fldRawValue.CanSet() {
 				if b != nil {
 					field.Set(reflect.ValueOf(&v))
